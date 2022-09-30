731d1ac7921c2c599fa63c5e91a9682ee652d87b
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2022-09-30 14:58:25 +0300

    shoppingcart_app
    shopping cart

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Macbook',
+            price: 10,
+            image: 'https://i.picsum.photos/id/727/400/600.jpg?hmac=vtgH-HPl5BJ2gxkoPbWto-6rYNHjNmco40yd-8uqhNQ'
+        },
+        {
+            id: '2',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://i.picsum.photos/id/727/400/600.jpg?hmac=vtgH-HPl5BJ2gxkoPbWto-6rYNHjNmco40yd-8uqhNQ'
+        },
+        {
+            id: '3',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://i.picsum.photos/id/727/400/600.jpg?hmac=vtgH-HPl5BJ2gxkoPbWto-6rYNHjNmco40yd-8uqhNQ'
+        },
+        {
+            id: '4',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://i.picsum.photos/id/727/400/600.jpg?hmac=vtgH-HPl5BJ2gxkoPbWto-6rYNHjNmco40yd-8uqhNQ'
+        },
+        {
+            id: '5',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://i.picsum.photos/id/727/400/600.jpg?hmac=vtgH-HPl5BJ2gxkoPbWto-6rYNHjNmco40yd-8uqhNQ'
+        },
+        {
+            id: '6',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://i.picsum.photos/id/727/400/600.jpg?hmac=vtgH-HPl5BJ2gxkoPbWto-6rYNHjNmco40yd-8uqhNQ'
+        },
+        
+    ],
+};
+export default data;
